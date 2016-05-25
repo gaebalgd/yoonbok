59a158814e052becdbed8e2f7fe15f9dc90fe28c
gaebalgd <inverliebt@gmail.com>
2016-05-25 16:21:28 +0900

    made blog app_2

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
-def index(request):
-	return render(request, 'blog.index.html')
+def post_list(request):
+	post_list = Post.objects.all()
+	return render(request, 'blog/post_list.html', {
+			'post_list': post_list,
+		})
+	
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {
+			'post': post,
+		})
